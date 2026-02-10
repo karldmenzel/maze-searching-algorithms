8883e5a71c6834c25067c1f3cbd0aff92c5aa1ed
karldmenzel <k.menzel15@gmail.com>
2026-02-09 23:03:59 -0600

    chore: add git ignore

--- informed.py
+++ informed.py
@@ -1,7 +1,6 @@
 def informed_search():
     print('Hello world from the informed search algorithm.')
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print('Beginning informed search.')
     informed_search()
--- uninformed.py
+++ uninformed.py
@@ -1,7 +1,6 @@
 def uninformed_search():
     print('Hello world from the uninformed search algorithm.')
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print('Beginning uninformed search.')
     uninformed_search()
